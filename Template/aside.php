be876b2b2aab1ef592dcad5b32d15106cc4a0d7f
Gino <ginovito@live.fr>
2018-11-06 15:01:52 +0100

    first commit

---
+++ Template/aside.php
@@ -0,0 +1,13 @@
+<aside class="col-lg-3">
+  <i class="fas fa-user-ninja fa-4x mb-3"></i>
+  <ul class="list-group">
+    <?php
+    //On boucle sur l'utilisateur stocké en session pour afficher toutes ses informations
+    foreach ($_SESSION["user"] as $key => $value) {
+      echo "<li class='list-group-item'>$key : $value</li>";
+    }
+    ?>
+    <a href="basket.php"class="list-group-item text-center"><i class="fas fa-shopping-basket"></i></a>
+    <a href="index.php"class="list-group-item text-center">Déconnecter</a>
+  </ul>
+</aside>
