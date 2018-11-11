aba56ec43d2daba9df2ed2b3c8ea2eaf433e858e
gino <ginovito@live.fr>
2018-11-11 18:40:35 +0100

    end

--- login.php
+++ login.php
@@ -1,30 +1,31 @@
 <?php
-//On charge le fichier avec les fonctions qui renvoient nos données
+//load page function
 require "Model/function.php";
-//On vérifie si le formulaire a été rempli
+//if the POST is present
 if(!empty($_POST)) {
-  //On nettoie les entrées du formulaire
+  //clean the entries of the form
   foreach ($_POST as $key => $value) {
     $_POST[$key] = htmlspecialchars($value);
   }
-  //On récupère les utilisateurs stockés sur le site (ici pour l'exercice ils sont stockés dans une fonction)
+  //var the board getUsers
   $users = getUsers();
 
-  //On vérifie si on trouve une correspondance avec les infromations du formulaire
+  //check if we find a correspondence with the form's infomations
   foreach ($users as $user) {
     if($user["name"] === $_POST["user_name"] && $user["password"] === $_POST["user_password"]) {
-      //Si c'est le cas on démarre une session pour y stocker les informations de l'utilisateur
+      //If so, start a session to store user information
       session_start();
       $_SESSION["user"] = $user;
+      // redirection on the products page
       header("Location: products.php");
-      //On met un exit pour arrêter l'execution du script, autrement la redirection n'aura pas le temps de se faire
       exit;
     }
   }
+  //if there are no users with that name
   header("Location: index.php?message=Nous n'avons aucun utilisateur avec ce nom ou ce mot de passe");
   exit;
 }
-//Si le formulaire n'est pas rempli on renvoie l'utilisateur sur la page de login avce un message
+//If the form is not filled
 else {
   header("Location: index.php?message=Vous devez remplir les champs du formulaire");
   exit;
